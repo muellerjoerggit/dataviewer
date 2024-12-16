7a70219544d440adbb3f2bf00ea33e98c2bf9c48
muellerjoerggit <mueller.joerg@outlook.com>
2024-12-16 22:15:28 +0100

    tests: new test for all entity types

---
+++ tests/EntityTypesTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests;
+
+use App\DaViEntity\Schema\EntityTypeSchemaRegister;
+use App\DaViEntity\Schema\EntityTypesRegister;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class EntityTypesTest extends KernelTestCase {
+
+  public function testSomething(): void {
+    $kernel = self::bootKernel();
+
+    $this->assertSame('test', $kernel->getEnvironment());
+
+    $entityTypesRegister = static::getContainer()->get(EntityTypesRegister::class);
+    $schemaRegister = static::getContainer()->get(EntityTypeSchemaRegister::class);
+
+    $entityTypes = [];
+    foreach($entityTypesRegister->iterateEntityTypes() as $entityType) {
+      $schema = $schemaRegister->getEntityTypeSchema($entityType);
+      $entityType = [
+        'type' => $entityType,
+        'label' => $schema->getEntityLabel()
+      ];
+
+      $entityTypes[] = $entityType;
+    }
+
+    $this->assertNotEmpty($entityTypes);
+
+  }
+}
