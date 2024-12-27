57a6e4bccd8537c65dd7c4ca02de1ae8d6e2172f
muellerjoerggit <mueller.joerg@outlook.com>
2024-12-27 07:48:47 +0100

    style: change arrangement constants

--- src/Services/AppNamespaces.php
+++ src/Services/AppNamespaces.php
@@ -7,13 +7,9 @@ class AppNamespaces {
   public const string ENTITY_TYPE_NAMESPACE = 'App\\DaViEntity\\EntityTypes';
 
   public const string NAMESPACE_ENTITY_REFERENCE_ITEM_HANDLER = 'App\\Item\\ItemHandler_EntityReference';
-
   public const string NAMESPACE_PRE_RENDERING_ITEM_HANDLER = 'App\\Item\\ItemHandler_PreRendering';
-
   public const string NAMESPACE_VALUE_FORMATTER_ITEM_HANDLER = 'App\\Item\\ItemHandler_ValueFormatter';
-
   public const string NAMESPACE_ADDITIONAL_DATA_ITEM_HANDLER = 'App\\Item\\ItemHandler_AdditionalData';
-
   public const string NAMESPACE_VALIDATOR_ITEM_HANDLER = 'App\\Item\\ItemHandler_Validator';
 
   public const string SQL_FILTER_HANDLER = 'App\\Database\\SqlFilterHandler';
