f11deea4b893a1752d0a2cf6cbd21dca583b1741
muellerjoerggit <mueller.joerg@outlook.com>
2025-02-02 23:45:46 +0100

    feat: extend logger

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -5,13 +5,19 @@ namespace App\Logger;
 use App\Logger\LogItems\LogItem;
 use App\Logger\LogItems\LogItemInterface;
 use Generator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Uid\Uuid;
 
-class Logger {
+class Logger implements LoggerInterface {
+
+
 
   private array $logs = [];
-
   private array $loggingContainer = [];
+
+  public function __construct(
+    private readonly LoggerInterface $logger,
+  ) {}
 
   public function startLoggingContainer(): string {
     $uuid = (string) Uuid::v7();
@@ -118,6 +124,42 @@ class Logger {
     foreach ($this->loggingContainer as $uuidContainer => $logs) {
       $this->loggingContainer[$uuidContainer][] = $uuid;
     }
+  }
+
+  public function emergency($message, array $context = []) {
+    $this->logger->emergency($message, $context);
+  }
+
+  public function alert($message, array $context = []) {
+    $this->logger->alert($message, $context);
+  }
+
+  public function critical($message, array $context = []) {
+    $this->logger->critical($message, $context);
+  }
+
+  public function error($message, array $context = []) {
+    $this->logger->error($message, $context);
+  }
+
+  public function warning($message, array $context = []) {
+    $this->logger->warning($message, $context);
+  }
+
+  public function notice($message, array $context = []) {
+    $this->logger->notice($message, $context);
+  }
+
+  public function info($message, array $context = []) {
+    $this->logger->info($message, $context);
+  }
+
+  public function debug($message, array $context = []) {
+    $this->logger->debug($message, $context);
+  }
+
+  public function log($level, $message, array $context = []) {
+    $this->logger->log($level, $message, $context);
   }
 
 }
