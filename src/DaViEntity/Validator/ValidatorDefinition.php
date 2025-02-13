e9d287a3d1be73fcb6db719f66b2aa4ee7e119ee
muellerjoerggit <mueller.joerg@outlook.com>
2025-02-13 21:46:18 +0100

    refactor: validator definition

--- src/DaViEntity/Validator/ValidatorDefinition.php
+++ src/DaViEntity/Validator/ValidatorDefinition.php
@@ -5,7 +5,7 @@ namespace App\DaViEntity\Validator;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_CLASS)]
-class ValidatorAttr {
+class ValidatorDefinition implements ValidatorDefinitionInterface {
 
   public function __construct(
     public readonly string $validatorClass,
